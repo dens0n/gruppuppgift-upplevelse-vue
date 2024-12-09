60f32617ce9e1949d71e925ee44a14ad45be061d
dens0n <156082076+dens0n@users.noreply.github.com>
2024-12-09 19:48:44 +0100

    search komponent

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -8,11 +8,8 @@ import { Bed } from "lucide-vue-next"
     <nav class="fixed flex h-20 w-full items-center justify-center bg-gray-300">
         <div class="container flex items-center justify-between px-4 xl:px-0">
             <Bed :size="30" />
-            <form action="submit">
-                <input type="text" />
-            </form>
             <ShoppingBasket :size="30" />
-            test
+        
         </div>
     </nav>
 </template>
