88093786e08490c3960e289b23a0951e98990b71
Jakob-mbi <jakob_mbi@msn.com>
2024-12-11 12:34:53 +0100

    fix color

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,7 +10,7 @@ import { RouterLink } from 'vue-router';
   <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
   <RouterLink to="/" class="flex items-center space-x-3 rtl:space-x-reverse">
     <Bed :size="30" />
-      <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Star Travel</span>
+      <span class="self-center text-2xl font-semibold whitespace-nowrap ">Star Travel</span>
   </RouterLink>
   <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
       <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center"><ShoppingBasket :size="30" /></button>
