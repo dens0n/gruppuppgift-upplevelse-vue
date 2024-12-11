82e38fc25cc796bbf1b36c6a94d456640377f231
dens0n <156082076+dens0n@users.noreply.github.com>
2024-12-11 12:20:58 +0100

    Refactor HomeView layout: restructure header for improved styling and integrate Search component within the header

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
 import Search from "@/components/Search.vue"
-
 </script>
 
 <template>
-  <main class="flex h-screen flex-col items-center justify-center bg-gradient-to-b from-slate-100 to-sky-500 p-4">
-    <header class="pb-16 text-center">
-      <h1 class="text-5xl font-extrabold text-gray-800 md:text-7xl">
-        Live like a local
-      </h1>
-      <h2 class="mt-4 text-4xl font-semibold text-gray-700 md:text-6xl">
-        Travel like a star
-      </h2>
+    <header
+        class="flex h-screen flex-col items-center justify-center bg-gradient-to-b from-slate-100 to-sky-500 p-4"
+    >
+        <div class="pb-16 text-center">
+            <h1 class="text-5xl font-extrabold text-gray-800 md:text-7xl">
+                Live like a local
+            </h1>
+            <h2 class="mt-4 text-4xl font-semibold text-gray-700 md:text-6xl">
+                Travel like a star
+            </h2>
+        </div>
+        <Search />
     </header>
-    <Search />
-  </main>
 </template>
