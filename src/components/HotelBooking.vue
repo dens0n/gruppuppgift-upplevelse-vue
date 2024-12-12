f2d70f10680558f12e1d90e19868305d4a1c3f24
JohanTran <johan.tran0210@gmail.com>
2024-12-12 12:58:06 +0100

    added dummy text for booking details

--- src/components/HotelBooking.vue
+++ src/components/HotelBooking.vue
@@ -29,18 +29,42 @@ watchEffect(async () => {
 
 <template>
     <div v-if="!loading">
-        <div class="container mx-auto">
+        <div class="container flex flex-col mx-auto gap-2">
             <img :src="`/img/${hotel.img}`" :alt="hotel.name" class="object-cover size-64 rounded-md">
-            <div class="flex w-full justify-between">
-                <h2 class="font-bold">{{ hotel.name }}, {{ hotel.city }}</h2>
-                <p><span class="font-bold">{{ hotel.price_per_night.adult }}kr SEK</span>/natt</p>
-            </div>
+            <h2 class="font-bold">{{ hotel.name }}, {{ hotel.city }}</h2>
+            <p>{{ hotel.beds }}</p>
+            <p>Antal gäster</p>
+            <p>Antal sovrum</p>
+            <p>Antal badrum</p>
+            <p>Antal</p>
+            <p><span class="font-bold">{{ hotel.price_per_night.adult }}kr SEK</span>/natt</p>
             <div class="mr-auto">
                 <p>Sammanlagda priset</p>
                 <p>Pris per person</p>
                 <p>Incheckning och utcheckning</p>
                 <p>Paketerbjudande</p>
+                <p>ingen fruktost och höghastighetsinternet</p>
+                <p>fruktost och höghastighetsinternet</p>
+                <p>fruktost och höghastighetsinternet och städservice</p>
+
             </div>
         </div>
     </div>
 </template>
+<!-- 
+"name": "Grand Plaza",
+"city": "Stockholm",
+"price_per_night": {
+    "adult": 1200,
+    "child": 800
+},
+"beds": 2,
+"img": "GrandPlaza.jpg" 
+        "essential_info": [
+            "Hel villa",
+            "Rymmer 4",
+            "1 sovrum",
+            "1 badrum",
+            "7 sängar"
+        ],
+-->
