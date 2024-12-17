0fb190f08076a07ad1c39f97ebd3c94dfce57372
JohanTran <johan.tran0210@gmail.com>
2024-12-17 19:32:07 +0100

    Ändra vdatepicker i hotelbooking Fixes #8

--- src/components/HotelBooking.vue
+++ src/components/HotelBooking.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { computed, ref, watchEffect } from 'vue';
-import { onBeforeRouteLeave, useRoute, useRouter } from 'vue-router';
-import { Wifi, EggFried, Brush, CirclePlus, CircleMinus } from 'lucide-vue-next';
+import { computed, reactive, ref, watchEffect } from 'vue';
+import { useRoute, useRouter } from 'vue-router';
+import { Wifi, EggFried, Brush, Plus, Minus } from 'lucide-vue-next';
 import type { SimpleDateRange } from 'v-calendar/dist/types/src/utils/date/range.js';
 import { useHotelStore } from '@/stores/hotel';
 
@@ -11,8 +11,6 @@ const hotel = ref<Hotel>({} as Hotel)
 const loading = ref<boolean>(false)
 
 const { name, from, to, adults, children } = route.params
-const adultCount = ref<number>(parseInt(adults.toString()))
-const childrenCount = ref<number>(parseInt(children.toString()))
 
 const router = useRouter();
 const isGuestModalOpen = ref(false)
@@ -21,21 +19,14 @@ const dateRange = ref<SimpleDateRange>({
     end: new Date(to.toString())
 })
 
-const increaseAdults = () => {
-    adultCount.value++
-}
+const guestCount = reactive({
+    adults: parseInt(adults.toString()),
+    children: parseInt(children.toString()),
+})
 
-const decreaseAdults = () => {
-    if (adultCount.value > 0) adultCount.value--
-}
-
-const increaseChildren = () => {
-    childrenCount.value++
-}
-
-const decreaseChildren = () => {
-    if (childrenCount.value > 0) childrenCount.value--
-}
+const totalGuests = computed(() => {
+    return guestCount.adults + guestCount.children;
+})
 
 const offers = ref<OfferType[]>([{
     icon: Wifi,
@@ -60,8 +51,8 @@ const createParams = computed(() => {
     const searchParams = {
         from: dateRange.value.start.toISOString().split("T")[0],
         to: dateRange.value.end.toISOString().split("T")[0],
-        adults: adultCount.value,
-        children: childrenCount.value,
+        adults: guestCount.adults,
+        children: guestCount.children,
     }
 
     return searchParams;
@@ -87,7 +78,7 @@ const checkall = () => {
 }
 
 const calculatePerNight = computed(() => {
-    return (hotel.value.price_per_night.adult * adultCount.value + hotel.value.price_per_night.child * childrenCount.value) * totalDays.value;
+    return (hotel.value.price_per_night.adult * guestCount.adults + hotel.value.price_per_night.child * guestCount.children) * totalDays.value;
 })
 
 const checkedOffers = computed(() => {
@@ -100,7 +91,7 @@ const checkedOffers = computed(() => {
 })
 
 const calculateOffers = computed(() => {
-    return checkedOffers.value.breakfast * (adultCount.value + childrenCount.value) + checkedOffers.value.wifi + checkedOffers.value.cleaning
+    return checkedOffers.value.breakfast * (guestCount.adults + guestCount.children) + checkedOffers.value.wifi + checkedOffers.value.cleaning
 })
 
 const calculateTotalPrice = computed(() => {
@@ -116,10 +107,23 @@ const convertToCheckout = computed<CheckoutType>(() => {
             start: dateRange.value.start.toISOString().split("T")[0],
             end: dateRange.value.end.toISOString().split("T")[0],
         },
-        adults: adultCount.value,
-        children: childrenCount.value
+        adults: guestCount.adults,
+        children: guestCount.children
     }
 })
+
+//Funktioner för antalet gäster
+const increaseGuestCount = (type: "adults" | "children") => {
+    if (guestCount[type] !== undefined && totalGuests.value < hotel.value.max_guests) {
+        guestCount[type]++
+    }
+    console.log(totalGuests.value)
+}
+const decreaseGuestCount = (type: "adults" | "children") => {
+    if (guestCount[type] !== undefined && guestCount[type] > 0 && totalGuests.value <= hotel.value.max_guests) {
+        guestCount[type]--
+    }
+}
 
 function handleCheckout() {
     store.addToCheckout(convertToCheckout.value)
@@ -165,71 +169,110 @@ watchEffect(() => {
             <div class="ml-auto space-y-2">
                 <p><span class="font-bold">Vuxen 18+ år {{ hotel.price_per_night.adult }}kr SEK</span>/natt</p>
                 <p><span class="font-bold">Barn 1-17 år {{ hotel.price_per_night.child }}kr SEK</span>/natt</p>
-                <div
-                    class="mx-5 flex h-[85px] w-full items-center justify-center divide-x rounded-full border border-gray-200 bg-white shadow-lg">
-
-                    <VDatePicker v-model.range="dateRange" mode="date">
-                        <template #default="{ inputValue, inputEvents }">
-                            <div
-                                class="flex h-full w-64 flex-col items-center justify-center text-start transition-transform duration-200 ease-in-out hover:rounded-full hover:bg-gray-200 hover:shadow-md focus:rounded-full focus:bg-gray-200 focus:shadow-sm focus:outline-none">
-                                <p>Incheckning</p>
-                                <input class="bg-transparent text-center font-extralight focus:outline-none "
-                                    :value="inputValue.start" v-on="inputEvents.start" readonly>
+                <div class="relative flex items-center justify-center">
+                    <div
+                        class="mx-5 flex h-[85px] w-full max-w-4xl items-center justify-center rounded-full border border-gray-200 bg-white shadow-lg">
+                        <VDatePicker v-if="hotel.available_dates" v-model.range="dateRange"
+                            :popover="{ placement: 'bottom' }" :min-date="hotel.available_dates.start"
+                            :max-date="hotel.available_dates.end">
+                            <template #default="{ inputValue, inputEvents }">
+                                <div class="flex h-full items-center justify-center gap-8 px-6 text-start transition-transform duration-200 ease-in-out hover:rounded-full hover:bg-gray-200 hover:shadow-md focus:rounded-full focus:bg-gray-200 focus:shadow-sm focus:outline-none"
+                                    :value="inputValue" v-on="inputEvents.start">
+                                    <div class="w-24">
+                                        <p class="text-center">Incheckning</p>
+                                        <input
+                                            class="w-full bg-transparent text-center font-extralight focus:outline-none"
+                                            readonly :value="inputValue.start" v-on="inputEvents.start" />
+                                    </div>
+                                    <div class="w-24">
+                                        <p class="text-center">Utcheckning</p>
+                                        <input
+                                            class="w-full bg-transparent text-center font-extralight focus:outline-none"
+                                            readonly :value="inputValue.end" v-on="inputEvents.end" />
+                                    </div>
+                                </div>
+                            </template>
+                        </VDatePicker>
+                        <div class="relative flex h-full w-64 flex-col justify-center transition-transform duration-200 ease-in-out hover:rounded-full hover:bg-gray-200 hover:shadow-md focus:rounded-full focus:bg-gray-200 focus:shadow-sm focus:outline-none"
+                            @mouseenter="isGuestModalOpen = true" @mouseleave="isGuestModalOpen = false">
+                            <div class="flex flex-col items-start">
+                                <span class="relative left-9">Vem</span>
+                                <span class="relative left-9 font-extralight">{{
+                                    guestCount.adults || guestCount.children > 0
+                                        ? guestCount.adults +
+                                        guestCount.children +
+                                        (guestCount.adults + guestCount.children === 1
+                                            ? " gäst"
+                                            : " gäster")
+                                        : "Lägg till gäster"
+                                }}</span>
                             </div>
-                            <div
-                                class="flex h-full w-64 flex-col items-center justify-center text-start transition-transform duration-200 ease-in-out hover:rounded-full hover:bg-gray-200 hover:shadow-md focus:rounded-full focus:bg-gray-200 focus:shadow-sm focus:outline-none">
-                                <p>Utcheckning</p>
-                                <input class="bg-transparent text-center font-extralight focus:outline-none"
-                                    :value="inputValue.end" v-on="inputEvents.end" readonly />
-                            </div>
-                        </template>
-                    </VDatePicker>
-                    <button
-                        class="relative h-full w-full transition-transform duration-200 ease-in-out hover:rounded-full hover:bg-gray-200 hover:shadow-md focus:rounded-full focus:bg-gray-200 focus:shadow-sm focus:outline-none"
-                        @mouseenter="isGuestModalOpen = true" @mouseleave="isGuestModalOpen = false">
-                        <div class="flex flex-col items-start">
-                            <span class="relative left-9">Vem</span>
-                            <span class="relative left-9 font-extralight">Lägg till gäster</span>
+                            <Transition enter-active-class="transition duration-200 ease-in-out"
+                                enter-from-class="opacity-0 scale-95 -translate-y-4"
+                                enter-to-class="opacity-100 scale-100 translate-y-0"
+                                leave-active-class="transition duration-200 ease-in-out"
+                                leave-from-class="opacity-100 scale-100 translate-y-0"
+                                leave-to-class="opacity-0 scale-95 -translate-y-4">
+                                <div v-if="isGuestModalOpen" class="absolute -bottom-[11px] right-1 h-3 w-[250px]">
+                                    <ul
+                                        class="absolute right-0 top-[11px] flex w-[250px] flex-col items-center justify-center divide-y rounded-lg border bg-white p-2 shadow-xl">
+                                        <li class="flex w-full items-center justify-between p-5">
+                                            <div>
+                                                <p>Vuxna</p>
+                                                <p class="text-sm font-extralight">
+                                                    13 år och äldre
+                                                </p>
+                                            </div>
+                                            <div class="flex items-center justify-center gap-1">
+                                                <button @click.stop="
+                                                    decreaseGuestCount('adults')
+                                                    ">
+                                                    <Minus :size="20" :color="guestCount.adults <= 0
+                                                        ? 'lightgray'
+                                                        : ''
+                                                        " :stroke-width="1" />
+                                                </button>
+                                                <span class="w-5 text-center">{{
+                                                    guestCount.adults
+                                                }}</span>
+                                                <button @click.stop="
+                                                    increaseGuestCount('adults')
+                                                    ">
+                                                    <Plus :size="20" :stroke-width="1" />
+                                                </button>
+                                            </div>
+                                        </li>
+                                        <li class="flex w-full items-center justify-between p-5">
+                                            <div>
+                                                <p>Barn</p>
+                                                <p class="text-sm font-extralight">
+                                                    Upp till 12 år
+                                                </p>
+                                            </div>
+                                            <div class="flex items-center justify-center gap-1">
+                                                <button @click.stop="
+                                                    decreaseGuestCount('children')
+                                                    ">
+                                                    <Minus :color="guestCount.children <= 0
+                                                        ? 'lightgray'
+                                                        : ''
+                                                        " :size="20" :stroke-width="1" />
+                                                </button>
+                                                <span class="w-5 text-center">{{
+                                                    guestCount.children
+                                                }}</span>
+                                                <button @click.stop="
+                                                    increaseGuestCount('children')
+                                                    ">
+                                                    <Plus :size="20" :stroke-width="1" />
+                                                </button>
+                                            </div>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </Transition>
                         </div>
-                        <Transition enter-active-class="transition duration-200 ease-in-out"
-                            enter-from-class="opacity-0 scale-95 -translate-y-4"
-                            enter-to-class="opacity-100 scale-100 translate-y-0"
-                            leave-active-class="transition duration-200 ease-in-out"
-                            leave-from-class="opacity-100 scale-100 translate-y-0"
-                            leave-to-class="opacity-0 scale-95 -translate-y-4">
-                            <div v-if="isGuestModalOpen" class="absolute -bottom-[11px] right-1 h-3 w-[350px]">
-                                <ul
-                                    class="absolute right-0 top-[11px] flex w-[350px] flex-col items-center justify-center divide-y rounded-lg border bg-white p-4 shadow-xl">
-                                    <li class="flex w-full items-center justify-between p-5">
-                                        <p>Vuxna</p>
-                                        <div class="flex items-center justify-center gap-2">
-                                            <button @click.stop="decreaseAdults">
-                                                <CircleMinus :size="30" :color="adultCount <= 0 ? 'lightgray' : ''
-                                                    " :stroke-width="1" />
-                                            </button>
-                                            <span class="w-5">{{ adultCount }}</span>
-                                            <button @click.stop="increaseAdults">
-                                                <CirclePlus :size="30" :stroke-width="1" />
-                                            </button>
-                                        </div>
-                                    </li>
-                                    <li class="flex w-full items-center justify-between p-5">
-                                        <p>Barn</p>
-                                        <div class="flex items-center justify-center gap-2">
-                                            <button @click.stop="decreaseChildren">
-                                                <CircleMinus :color="childrenCount <= 0 ? 'lightgray' : ''
-                                                    " :size="30" :stroke-width="1" />
-                                            </button>
-                                            <span class="w-5">{{ childrenCount }}</span>
-                                            <button @click.stop="increaseChildren">
-                                                <CirclePlus :size="30" :stroke-width="1" />
-                                            </button>
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                        </Transition>
-                    </button>
+                    </div>
                 </div>
 
                 <div class="flex border-b border-gray-300">
@@ -247,9 +290,9 @@ watchEffect(() => {
                     </div>
                     <div class="w-full flex flex-col">
                         <div class="flex justify-between max-w-xs">
-                            <p>{{ hotel.price_per_night.child * childrenCount + hotel.price_per_night.adult * adultCount
-                                }}
-                                x {{ totalDays }} nätter</p>
+                            <p>{{ hotel.price_per_night.child * guestCount.children + hotel.price_per_night.adult *
+                                guestCount.adults
+                                }} x {{ totalDays }} nätter</p>
                             <p><span class="font-bold">{{ calculatePerNight }}kr SEK</span></p>
                         </div>
                         <div class="flex justify-between max-w-xs">
